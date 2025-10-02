33c9f92aff3fc350d74eb78e32e43392e7f79aed
Mehdi Serghini <91221718+Serghini04@users.noreply.github.com>
2025-10-02 20:59:54 +0000

    feat: add main application layout with sidebar and navigation

---
+++ app/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import MyHeader from "./components/MyHeader";
+import SideMenu from "./components/SideMenu";
+
+function App() {
+
+  return (
+    <div>
+      <MyHeader />
+      <SideMenu />
+    </ div>
+  );
+}
+
+export default App
---
+++ app/frontend/src/components/SideMenu.tsx
@@ -0,0 +1,8 @@
+
+export default function SideMenu() {
+    return (
+        <div>
+            <h1> === SideMenu === </h1>
+        </div>
+    );
+}
